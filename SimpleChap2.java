74dbd3696960077266b870c15e163d4aedeea71d
lhamdou777 <58756918+lhamdou777@users.noreply.github.com>
2020-02-04 10:41:29 +0000

    Add files via upload
    version 2 of SimpleChap.java and WallDetection.java version 1

---
+++ SimpleChap2.java
@@ -0,0 +1,37 @@
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.SampleProvider;
+
+public class SimpleChap2 {
+    public static void main(String[] args) {
+        BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
+        BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.B);
+        mLeft.setSpeed(720);
+        mRight.setSpeed(720);
+        
+        Thread thread = new WallDetection (mLeft, mRight);
+        thread.start();
+        
+        
+        float[] level = new float[1]; // A sound sample is just one number
+        NXTSoundSensor ss = new NXTSoundSensor(SensorPort.S1);
+        SensorMode sound = (SensorMode) ss.getDBAMode();
+        SampleProvider clap = new ClapFilter(sound , 0.6f, 1000);
+        clap.fetchSample(level,0);
+
+        if (level[0] > 0.9f) {
+            while(true) {
+                mLeft.forward();
+                mRight.forward();
+                clap.fetchSample(level, 0);
+                if (level[0] > 0.9f) {
+                    mLeft.rotate(90);
+                }
+            }
+        }
+    }
+}
---
+++ WallDetection.java
@@ -0,0 +1,27 @@
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
+public class WallDetection extends Thread {
+	private BaseRegulatedMotor mLeft;
+	private BaseRegulatedMotor mRight;
+	private float[] arr;
+	
+	public WallDetection(BaseRegulatedMotor m, BaseRegulatedMotor r) {
+		this.mLeft = m;
+		this.mRight = r;
+	}
+	
+	public void detectWall() {
+		EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S2);
+		SampleProvider samps = sensor.getDistanceMode();
+		while(true) {
+			samps.fetchSample(arr, 0);
+			if(arr[0] < 0.5f ) {
+				mLeft.stop();
+				mRight.stop();
+			}
+		}
+	}
+}
