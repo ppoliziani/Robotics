544a7b34a9b2b4d449f04153829a664c6c5984dc
lhamdou777 <58756918+lhamdou777@users.noreply.github.com>
2020-02-21 10:47:18 +0000

    Add files via upload
    Qr code reader function

---
+++ Project/ReadQrCode.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+package Project;
+
+public class ReadQrCode {
+	
+	public static String readQRCode(String filePath, String charset, Map hintMap)throws FileNotFoundException, IOException, NotFoundException {
+		    
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+		new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filePath)))));
+		    Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,hintMap);
+		    return qrCodeResult.getText();
+	}
+
+}
