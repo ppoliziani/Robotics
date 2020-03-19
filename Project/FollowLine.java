2872b7c55f8e1803eaf5a858329121ab56b1366d
ppoliz <49439911+ppoliziani@users.noreply.github.com>
2020-03-19 18:48:16 +0000

    Add files via upload

--- Project/FollowLine.java
+++ Project/FollowLine.java
@@ -4,12 +4,19 @@ import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
 
 public class FollowLine implements Behavior {
+	
+	private int AVOID_THIS_DISTANCE = 15;
+	
+	private EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(SensorPort.S4);
+	private SampleProvider sp = ultraSensor.getDistanceMode();
+	private float[] distance = new float[1];
 	
     private MovePilot pilot;
     private Thread QRwatcher;
@@ -21,16 +28,20 @@ public class FollowLine implements Behavior {
 	}
 	
 	public void action() {
-		pilot.forward();
 		QRwatcher.start();
 		Colourwatcher.start();
+		pilot.forward();
 	}
 
 	public void suppress() {}
 	
 	public boolean takeControl() {
 		Connection con = new Connection();
-		return (con.getConnected());
+		
+		sp.fetchSample(distance, 0);
+		return (con.getConnected() | distance[0] < AVOID_THIS_DISTANCE);
+	
+		
 	}
 	
 
