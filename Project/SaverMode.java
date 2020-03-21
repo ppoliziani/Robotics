caebca107f1fdbea15639390303ae635b5c95354
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2020-03-21 15:44:29 +0000

    Update SaverMode.java

--- Project/SaverMode.java
+++ Project/SaverMode.java
@@ -1,12 +1,20 @@
 import lejos.hardware.Battery;
 import lejos.hardware.lcd.LCD;
-import lejos.robotics.navigation.MovePilot;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.robotics.subsumption.Behavior;
 
 class SaverMode implements Behavior {
     private final float LOW_LEVEL;
-    public MovePilot pilot;
-    public SaverMode(MovePilot p) { this.pilot = p; }
+    private EV3LargeRegulatedMotor mLeft;
+    private EV3LargeRegulatedMotor mRight;
+    
+    public SaverMode(EV3LargeRegulatedMotor left, EV3LargeRegulatedMotor right) { 
+        this.left = mLeft;
+        this.right = mRight;
+        mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight});
+    }
+    
     public void action() {
         if (takeControl()) {
             LCD.drawString("Saver mode enabled", 0, 0);
